1c9e0f47b08ab3a86595767a3d6440dd75614d02
b-b-blueberry <pocketsathome@gmail.com>
2021-09-04 22:21:23 +1000

    Add project files.

---
+++ InstantPets/InstantPets/IGenericModConfigMenuAPI.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+using System;
+
+namespace InstantPets
+{
+	public interface IGenericModConfigMenuAPI
+	{
+		void RegisterModConfig(IManifest mod, Action revertToDefault, Action saveToFile);
+		void SetDefaultIngameOptinValue(IManifest mod, bool optedIn);
+		void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func<bool> optionGet, Action<bool> optionSet);
+	}
+}
